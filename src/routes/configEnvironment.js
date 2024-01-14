2c3eadc8c86169836b6760810ed375d757101f2f
Sofiane Belaribi <belaribi.sofiane@gmail.com>
2024-01-14 14:28:02 +0100

    change VITE_CRYPTED_MODE_KEY

--- src/routes/configEnvironment.js
+++ src/routes/configEnvironment.js
@@ -2,5 +2,6 @@ export let cryptedModeKey = ''
 if (process.env.NODE_ENV == 'development') {
 	cryptedModeKey = import.meta.env.VITE_CRYPTED_MODE_KEY;
 } else {
-	cryptedModeKey = process.env.VITE_CRYPTED_MODE_KEY;
+	cryptedModeKey = import.meta.env.VITE_CRYPTED_MODE_KEY;
+	// cryptedModeKey = process.env.VITE_CRYPTED_MODE_KEY;
 }
