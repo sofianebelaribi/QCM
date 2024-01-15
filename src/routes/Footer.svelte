bd4b648f832a5d3eb4636ca2475a106bd0ba6fa8
Sofiane Belaribi <belaribi.sofiane@gmail.com>
2024-01-15 19:46:59 +0100

    Adding more QCMs

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -10,6 +10,9 @@
 	<div class="content has-text-centered is-size-6">
 				<p>If you have any questions, please send an email to: <a href="mailto:sofiane5434@gmail.com">sofiane5434@gmail.com</a></p>
 				<p><a href="https://qcm-five.vercel.app/#https://raw.githubusercontent.com/sofianebelaribi/QCM/master/QCM1.txt" target="_blank">QCM Anapath</a></p>
+				<p><a href="https://qcm-five.vercel.app/#https://raw.githubusercontent.com/sofianebelaribi/QCM/master/QCM2.txt" target="_blank">QCM Ergo</a></p>
+				<p><a href="https://qcm-five.vercel.app/#https://raw.githubusercontent.com/sofianebelaribi/QCM/master/QCM3.txt" target="_blank">QCM Semiologie</a></p>
+				<p><a href="https://qcm-five.vercel.app/#https://raw.githubusercontent.com/sofianebelaribi/QCM/master/QCM4.txt" target="_blank">QCM Pharmaco</a></p>
 	</div>
 </footer>
 
